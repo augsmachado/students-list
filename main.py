6e3e0b09d80849dcc9d7f6d823ca469d091aa813
Augs Machado <augustomachadoramos@gmail.com>
2025-02-21 19:05:46 -0300

    [3] Add student retrieval by name endpoint and rename existing function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI, Path
 
 app = FastAPI()
@@ -27,10 +28,20 @@ def index():
 
 # Path parameters
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(description="The ID student that you want to view", gt=0)):
+def get_student_by_id(student_id: int = Path(description="The ID student that you want to view", gt=0)):
     if student_id not in students:
-        return {
-            "message": "Student not found"
-        }
+        return {"message": "Student not found"}
 
     return students[student_id]
+
+# Query parameters
+@app.get("/get-by-name")
+def get_student_by_name(*, name: Optional[str] = None):
+    return next(
+        (
+            students[student_id]
+            for student_id in students
+            if students[student_id]["name"] == name
+        ),
+        {"message": "Student not found"},
+    )
