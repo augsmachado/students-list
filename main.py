09e141d4c845b223def2b551eafc10dd8abeb987
Augs Machado <augustomachadoramos@gmail.com>
2025-02-21 18:36:54 -0300

    [2] Add student retrieval endpoint with path parameter validation

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
+
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "class": "year 12"
+    }
+}
+
+
 
 @app.get("/status")
 def status():
@@ -14,3 +24,13 @@ def index():
     return {
         "name": "First Data"
     }
+
+# Path parameters
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(description="The ID student that you want to view", gt=0)):
+    if student_id not in students:
+        return {
+            "message": "Student not found"
+        }
+
+    return students[student_id]
